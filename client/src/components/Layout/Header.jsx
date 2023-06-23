7d127e37fbcdd4648bc3c002383a4d282b1600ce
Azizul Hakim <azizulhakimgps@gmail.com>
2023-06-24 01:47:06 +0600

    commit

---
+++ client/src/components/Layout/Header.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import {NavLink, Link} from "react-router-dom";
+import {useAuth} from "../../context/auth";
+import {useCart} from "../../context/cart";
+import {Badge, message} from "antd";
+
+const Header = () => {
+    const [auth, setAuth] = useAuth();
+    const [cart] = useCart();
+    const handleLogout = () => {
+        setAuth({
+            ...auth,
+            user: null,
+            token: ""
+        });
+        localStorage.removeItem("auth");
+        message.success("Logout Successfully");
+    };
+    return (
+        <>
+            <nav className="bg-headingColor border-gray-200 dark:bg-gray-900 px-5">
+                <div className="max-w-screen flex flex-wrap items-center justify-between mx-auto p-4">
+                    <Link to="/" className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+                        🛍️REPLIQ
+                    </Link>
+                    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-headingColor dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+
+                            <li className="nav-item">
+                                <NavLink to="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 ">
+                                    Home
+                                </NavLink>
+                            </li>
+
+                            {
+                            !auth ?. user ? (
+                                <>
+                                    <li className="nav-item">
+                                        <NavLink to="/register" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">
+                                            Register
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/login" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">
+                                            Login
+                                        </NavLink>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li >
+                                        <NavLink id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-white-900 rounded  md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto text-white md:dark:hover:text-blue-500 dark:focus:text-white md:dark:hover:bg-transparent">
+                                            {
+                                            auth ?. user ?. name
+                                        } </NavLink>
+                                        <div id="dropdownNavbar" className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                                                <li>
+                                                    <NavLink to={
+                                                            `/dashboard/${
+                                                                auth ?. user ?. role === 1 ? "admin" : "user"
+                                                            }`
+                                                        }
+                                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                        Dashboard
+                                                    </NavLink>
+                                                </li>
+                                                <li>
+                                                    <NavLink onClick={handleLogout}
+                                                        to="/login"
+                                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                        Logout
+                                                    </NavLink>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </>
+                            )
+                        }
+                            <li className="nav-item">
+                                <NavLink to="/cart" className="block py-2 pl-3 pr-4 rounded  md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-blue-700 hover:text-white md:dark:hover:bg-transparent">
+                                    <Badge count={
+                                            cart ?. length
+                                        }
+                                        showZero
+                                        offset={
+                                            [10, -5]
+                                    }>
+                                        Cart
+                                    </Badge>
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Header;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Checkbox, Radio, message } from "antd";
+import { useCart } from "../context/cart";
+import axios from "axios";
+import Layout from "./../components/Layout/Layout";
+import { AiOutlineReload } from "react-icons/ai";
+import "../styles/Homepage.css";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  const [cart, setCart] = useCart();
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+
+  useEffect(() => {
+
+    getTotal();
+  }, []);
+  //get products
+  const getAllProducts = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`http://localhost:8080/api/v1/product/product-list/${page}`);
+      setLoading(false);
+      setProducts(data.products);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  //getTOtal COunt
+  const getTotal = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:8080/api/v1/product/product-count");
+      setTotal(data?.total);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (page === 1) return;
+    loadMore();
+  }, [page]);
+  //load more
+  const loadMore = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`http://localhost:8080/api/v1/product/product-list/${page}`);
+      setLoading(false);
+      setProducts([...products, ...data?.products]);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!checked.length || !radio.length) getAllProducts();
+  }, [checked.length, radio.length]);
+  
+  return (
+    <Layout>
+      {/* banner image */}
+      <img
+        src="/favicon.png"
+        className="banner-img"
+        alt="bannerimage"
+        width={"5%"}
+      />
+      {/* banner image */}
+      <div className="container-fluid row mt-3 home-page">
+        
+        <div className="col-md-9 ">
+          <h1 className="text-center">All Products</h1>
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card m-2" key={p._id}>
+                <img
+                  src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <div className="card-name-price">
+                    <h5 className="card-title">{p.name}</h5>
+                    <h5 className="card-title card-price">
+                      {p.price.toLocaleString("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                      })}
+                    </h5>
+                  </div>
+                  <p className="card-text ">
+                    {p.description.substring(0, 60)}...
+                  </p>
+                  <div className="card-name-price">
+                    <button
+                      className="btn btn-dark ms-1"
+                      onClick={() => {
+                        setCart([...cart, p]);
+                        localStorage.setItem(
+                          "cart",
+                          JSON.stringify([...cart, p])
+                        );
+                        message.success("Item Added to cart");
+                      }}
+                    >
+                      ADD TO CART
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="m-2 p-3">
+            {products && products.length < total && (
+              <button
+                className="btn loadmore"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setPage(page + 1);
+                }}
+              >
+                {loading ? (
+                  "Loading ..."
+                ) : (
+                  <>
+                    {" "}
+                    Loadmore <AiOutlineReload />
+                  </>
+                )}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default HomePage;
