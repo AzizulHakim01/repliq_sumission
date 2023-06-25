51cf223c1e247d76af3c04a4c3ccda9e3e54a1d6
Azizul Hakim <azizulhakimgps@gmail.com>
2023-06-25 18:24:26 +0600

    bug fixed
    Fixed Bug in Admin/Product

--- client/src/pages/Admin/Products.jsx
+++ client/src/pages/Admin/Products.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "./../../components/Layout/Layout";
 import axios from "axios";
+import BASE_URL from "../../api"
 import { Link } from "react-router-dom";
 import { message } from "antd";
 const Products = () => {
@@ -10,7 +11,7 @@ const Products = () => {
   //getall products
   const getAllProducts = async () => {
     try {
-      const { data } = await axios.get("http://localhost:8080/api/v1/product/get-product");
+      const { data } = await axios.get(`${BASE_URL}/v1/product/get-product`);
       setProducts(data.products);
     } catch (error) {
       console.log(error);
@@ -39,7 +40,7 @@ const Products = () => {
               >
                 <div className="card m-2" style={{ width: "18rem" }}>
                   <img
-                    src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                    src={`${BASE_URL}/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                   />
